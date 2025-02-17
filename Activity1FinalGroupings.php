40ec527ba433aed291b3b789bc2617057386a8a9
Juls-33 <juliusaustin.santos@gmail.com>
2025-02-17 22:04:16 +0800

    updated

--- Activity1FinalGroupings.php
+++ Activity1FinalGroupings.php
@@ -115,11 +115,11 @@
                     width: 600,
                     padding: "3em",
                     color: "#716add",
-                    background: "#fff url(/images/trees.png)",
+                    background: "url(pictures/swalbg6.jpg)",
                     backdrop: `
                         rgba(0,0,123,0.4)
-                        url("/images/nyan-cat.gif")
-                        left top
+                        url("")
+                        center
                         no-repeat
                     `
                     });
