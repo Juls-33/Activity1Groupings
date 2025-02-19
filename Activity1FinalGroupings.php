faab171472a480e9d6352ffd28a4499aa7fff749
Juls-33 <juliusaustin.santos@gmail.com>
2025-02-19 08:58:34 +0800

    updated

--- Activity1FinalGroupings.php
+++ Activity1FinalGroupings.php
@@ -55,13 +55,21 @@
                 <h1>Personal Information Description</h1>
                 <p  id="description">Welcome! Please provide your details below to help us better understand your needs. <b>All fields are required.</b></p>
                 <form>
-                    <label for="fName">First Name:</label>
+                    <div class="label-container">
+                        <label for="fName">First Name:</label>
+                        <p id="count-result-fName" class="counter">Character Count: 0/100</p>
+                    </div>
+                    
                     <br>
-                    <input class="inputDesign" type="text" id="fName" name="fName" placeholder="Example: John Doe" required>
+                    <input class="inputDesign formInput" type="text" id="fName" name="fName" placeholder="Example: John Doe" maxLength='100' required>
+                    
                     <br>
+                    <div class="label-container">
                     <label for="lName">Last Name:</label>
+                        <p id="count-result-lName" class="counter">Character Count: 0/100</p>
+                    </div>
                     <br>
-                    <input class="inputDesign" type="text" id="lName" name="lName" placeholder="Example: James" required>
+                    <input class="inputDesign" type="text" id="lName" name="lName" placeholder="Example: James" maxLength='100' required>
                     <br>
            
                     <p id="sex">Please select your gender:</p>
@@ -76,12 +84,16 @@
            
                     <label for="userAge">Age:</label>
                     <br>
-                    <input class="inputDesign" type="number" id="userAge" name="userAge" placeholder="Example: 18" required>
+                    <input class="inputDesign" type="number" id="userAge" name="userAge" placeholder="Example: 18" onkeypress="return event.charCode>=48 && event.charCode<=57" required>
                     <br>
-           
-                    <label for="userAddress">Address:</label>
+
+                    <div class="label-container">
+                        <label for="userAddress">Address:</label>
+                        <p id="count-result-uAdd" class="counter">Character Count: 0/150</p>
+                    </div>
+                    
                     <br>
-                    <input class="inputDesign address" type="text" id="userAddress" name="userAddress" placeholder="Example: 123, LeBron Street, Barangay James, Los Angeles City" required>
+                    <input class="inputDesign address" type="text" id="userAddress" name="userAddress" placeholder="Example: 123, LeBron Street, Barangay James, Los Angeles City" maxLength='150' required>
                     <br>
                     <br>
                    
@@ -201,24 +213,15 @@
         //check for error function
         function isError(formData){
             var errorString = ""
-            if (formData.user_fName.length>=100) {
-                errorString +="--First name is too long--\n";
-                document.getElementById("fName").classList.add("inputDesignError");
-            }else if (!formData.user_fName) {
+            if (!formData.user_fName) {
                 errorString +="--First name is empty--\n";
                 document.getElementById("fName").classList.add("inputDesignError");
             }
-            if(formData.user_lName.length>=100){
-                errorString +="--Last name is too long--\n";
-                document.getElementById("lName").classList.add("inputDesignError");
-            }else if(!formData.user_lName){
+            if(!formData.user_lName){
                 errorString +="--Last name is empty--\n";
                 document.getElementById("lName").classList.add("inputDesignError");
-            }"inputDesignError"
-;            if(formData.user_address.length>=150){
-                errorString +="--Address is too long--\n";
-                document.getElementById("userAddress").classList.add("inputDesignError");
-            }else if(!formData.user_address){
+            }
+            if(!formData.user_address){
                 errorString +="--Address is empty--\n";
                 document.getElementById("userAddress").classList.add("inputDesignError");
             }
@@ -228,21 +231,31 @@
             if(!formData.user_age){
                 errorString +="--Age is empty/invalid--\n";
                 document.getElementById("userAge").classList.add("inputDesignError");
-            }else if(formData.user_age % 1 != 0){
-                errorString +="--Age should not have a decimal--\n";
-                document.getElementById("userAge").classList.add("inputDesignError"); 
             }
-            if(formData.user_age<0){
-                errorString +="--Age should not be negative--\n";
-                document.getElementById("userAge").classList.add("inputDesignError");
-            }
-
             if(formData.user_age>125){
-                errorString +="--Age is too big--\n";
+                errorString +="--Age is invalid--\n";
                 document.getElementById("userAge").classList.add("inputDesignError");
             }
             return errorString;
         }
+        //text counter
+        let input = document.querySelector('#fName');
+        let input1 = document.querySelector('#lName');
+        let input2 = document.querySelector('#userAddress');
+        input.addEventListener('keyup', (e) => {
+            counter = document.querySelector('#count-result-fName');
+            maxLength = 100;
+            counter.innerHTML = `Character Count: ${e.target.value.length}/${maxLength}`
+        })
+        input1.addEventListener('keyup', (e) => {
+            counter = document.querySelector('#count-result-lName');
+            maxLength = 100;
+            counter.innerHTML = `Character Count: ${e.target.value.length}/${maxLength}`
+        })
+        input2.addEventListener('keyup', (e) => {
+            counter = document.querySelector('#count-result-uAdd');
+            counter.innerHTML = `Character Count: ${e.target.value.length}/${maxLength}`
+        })
     </script>
 </body>
 </html>
